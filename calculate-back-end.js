e34d0661105b1dd34d373b5974178452e6df2d1f
JumarySanchez <sanchezjumary@gmail.com>
2020-06-23 09:57:45 -0400

    calculate

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,7 +1,27 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
+function calculate(num1,num2,param){
+  
+  num1= Number(num1)
+  num2= Number(num2)
+  if (param === '+' || param === 'plus' || param === 'added to')
+  {
+    return num1+num2
+  } else if (param === '-' || param === 'minus' || param === 'subtracted from'){
+    return num1-num2
+  }else if (param === 'x' || param === 'times' || param === 'X' || param === 'multiplied by' ){
+    return num1*num2
+  }else if (param === '/' || param === 'divided by'){
+    return num1/num2
+  }else if (param === '%' || param == 'modulus' || param === 'mod'){
+    return num1%num2
+  }
+  else {
+    return "Sorry, that's not a mathematical operation!"
+  }
+  }
+  
 
  
 
